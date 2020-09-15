fd044d4f5a203bade918d2f9fa03808768b2cd91
gzd0392 <65642685+gzd0392@users.noreply.github.com>
2020-09-15 20:55:02 +0800

    Add files via upload

---
+++ 05_practice_02.ino
@@ -0,0 +1,10 @@
+int ledPin = 7;
+void setup() {
+       pinMode(ledPin, OUTPUT);
+}
+void loop() {
+       digitalWrite(ledPin,HIGH);
+       delay(200);
+       digitalWrite(ledPin,LOW);
+       delay(200);
+}
